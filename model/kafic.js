a4ea0f950f1a5fccb758a1edcf60f577231d16a5
AntonelaS <92169056+AntonelaS@users.noreply.github.com>
2023-03-11 19:37:35 +0100

    Dodan model koji prikazuje kako će kafić izgledat.

---
+++ model/kafic.js
@@ -0,0 +1,11 @@
+class Kafic {
+    constructor(id,ime_kafica,grad,satnica,trazeno_mjesto,broj){
+      this.id=id
+      this.ime_kafica=ime_kafica
+      this.grad=grad
+      this.satnica=satnica
+      this.trazeno_mjesto=trazeno_mjesto
+      this.broj=broj
+    }
+  }
+  export default Kafic
