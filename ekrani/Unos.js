2fc9c4df44a7b89824d2695ab7861af9aa469606
AntonelaS <92169056+AntonelaS@users.noreply.github.com>
2023-03-12 16:49:46 +0100

    Update Unos.js
    Napravljen ekran za unos nove ponude za posao.

--- ekrani/Unos.js
+++ ekrani/Unos.js
@@ -1,13 +1,125 @@
 import React, { useState } from 'react';
-import {
-  View, 
-  Text} from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
+
+import Kafic from '../model/kafic';
+import { KAFICI } from '../podatci/pocetni_podatci';
+
+import Tipka from '../components/Tipka'
 
 const Unos = () => {
+  const [ime, postaviIme] = useState('');
+  const [grad, postaviGrad] = useState('');
+  const [satnica, postaviSatnicu] = useState(4.38);
+  const [mjesto, postaviMjesto] = useState('');
+  const [broj, postaviBroj] = useState(1);
+
+  const changeIme = (tekst) => {
+    postaviIme(tekst);
+  };
+  const changeGrad = (tekst) => {
+    postaviGrad(tekst);
+  };
+  const changeSatnicu = (tekst) => {
+    postaviSatnicu(tekst);
+  };
+  const changeMjesto = (tekst) => {
+    postaviMjesto(tekst);
+  };
+  const changeBroj = (tekst) => {
+    postaviBroj(tekst);
+  };
+
+  const dodajNovi = () => {
+    if (
+      ime === '' ||
+      grad === '' ||
+      satnica < 4.38 ||
+      mjesto === '' ||
+      broj < 1
+    ) {
+      return;
+    } else {
+      const novi = new Kafic(KAFICI.length, ime, grad, satnica, mjesto, broj);
+      KAFICI.push(novi);
+      postaviIme('');
+      postaviGrad('');
+      postaviSatnicu(4.38);
+      postaviMjesto('');
+      postaviBroj(1);
+    }
+  };
+
   return (
-    <View>
-      <Text>Ekran za unos</Text>
+    <View style={stil.ekran}>
+      <View>
+        <Text style={{ color: 'black' }}>Ime kafića:</Text>
+        <TextInput style={stil.tekst} value={ime} onChangeText={changeIme} />
+      </View>
+
+      <View>
+        <Text style={{ color: 'black' }}>Grad:</Text>
+        <TextInput
+          style={stil.tekst}
+          value={grad}
+          onChangeText={changeGrad}
+        />
+      </View>
+
+      <View>
+        <Text style={{ color: 'black' }}>Satnica:</Text>
+        <TextInput
+          style={stil.tekst}
+          value={satnica}
+          onChangeText={changeSatnicu}
+        />
+      </View>
+
+      <View>
+        <Text style={{ color: 'black' }}>Djelatnost:</Text>
+        <TextInput
+          style={stil.tekst}
+          value={mjesto}
+          onChangeText={changeMjesto}
+        />
+      </View>
+
+      <View>
+        <Text style={{ color: 'black' }}>Broj potrebnih djelatnika(ica):</Text>
+        <TextInput
+          style={stil.tekst}
+          value={broj}
+          onChangeText={changeBroj}
+        />
+      </View>
+      
+      <Tipka onPress={dodajNovi}>Dodaj novu ponudu</Tipka>
+      
     </View>
   );
 };
+
+const stil = StyleSheet.create({
+  ekran: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tekst: {
+    height: 50,
+    width: 185,
+
+    padding: 10,
+
+    borderWidth: 1,
+
+    color: 'black',
+
+    borderColor: 'black',
+    borderRadius: 5,
+
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+});
+
 export default Unos;
