a1ba931045542a0230e81b064b5d7d853976e110
AntonelaS <92169056+AntonelaS@users.noreply.github.com>
2023-03-14 19:38:04 +0100

    Dodan dio za favorite.

--- ekrani/Naslovna.js
+++ ekrani/Naslovna.js
@@ -1,29 +1,31 @@
 import * as React from 'react';
-import {
-  View,
-  StyleSheet,
-  Text,
-  Image} from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
 import Tipka from '../components/Tipka';
 
 import slika from '../assets/kafici.JPG';
 
-const Naslovna = ({props, navigation}) => {
+const Naslovna = ({ props, navigation }) => {
   return (
     <View style={stil.ekran}>
-       <Image 
-       source={slika} 
-       style={{width:150, height:150}} 
-       />
+      <Image source={slika} style={{ width: 150, height: 150 }} />
 
       <Text style={stil.upute_naslov}>{'UPUTE: '}</Text>
       <Text style={stil.upute_tekst}>
-        {'Ako želiš pronaći posao u kafiću ili ako želiš ponuditi posao u kafiću koji nitko neće moći odbiti onda je ovo stranica za tebe!!!'}
+        {
+          'Ako želiš pronaći posao u kafiću ili ako želiš ponuditi posao u kafiću koji nitko neće moći odbiti onda je ovo stranica za tebe!!!'
+        }
       </Text>
       <View style={stil.tipke}>
-        <Tipka onPress={() => navigation.navigate("Pregled ponuda")}>Pregled ponuda</Tipka>
-        <Tipka onPress={() => navigation.navigate("Unos nove ponude")}>Unos ponude</Tipka>
+        <Tipka onPress={() => navigation.navigate('Pregled ponuda')}>
+          Pregled ponuda
+        </Tipka>
+        <Tipka onPress={() => navigation.navigate('Moji favoriti')}>
+          Moji favoriti
+        </Tipka>
+        <Tipka onPress={() => navigation.navigate('Unos nove ponude')}>
+          Unos ponude
+        </Tipka>
       </View>
     </View>
   );
@@ -39,17 +41,17 @@ const stil = StyleSheet.create({
     color: 'black',
     fontSize: 20,
     textAlign: 'center',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   upute_tekst: {
     color: 'black',
     padding: 20,
     fontSize: 18,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   tipke: {
-    flexDirection: 'row'
-  }
+    flexDirection: 'row',
+  },
 });
 
 export default Naslovna;
