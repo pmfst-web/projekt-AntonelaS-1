43bb56b36973494b8fc1aa20d0283d1378e8a5fc
AntonelaS <92169056+AntonelaS@users.noreply.github.com>
2023-03-18 19:53:35 +0100

    Dodan novi model kako će izgledati ponude.

---
+++ model/ponuda.js
@@ -0,0 +1,11 @@
+class Ponuda {
+  constructor(id, ime, mjesto, satnica, pozicija, broj) {
+    this.id = id;
+    this.ime = ime;
+    this.mjesto = mjesto;
+    this.satnica = satnica;
+    this.pozicija = pozicija;
+    this.broj = broj;
+  }
+}
+export default Ponuda;
